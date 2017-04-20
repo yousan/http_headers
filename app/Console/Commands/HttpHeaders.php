1f00790e34a106dca97c96ea8bd587e59856783a
Yousan_O <yousan@l2tp.org>
2017-04-20 15:52:18 +0900

    fixing!

--- app/Console/Commands/HttpHeaders.php
+++ app/Console/Commands/HttpHeaders.php
@@ -2,7 +2,10 @@
 
 namespace App\Console\Commands;
 
+use Exception;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Psr7\Response;
 use Illuminate\Console\Command;
 
 class HttpHeaders extends Command
@@ -43,24 +46,42 @@ class HttpHeaders extends Command
     {
 	    $name = $this->argument("name");
 	    $fetch_headers_str = $this->option('fetch-headers');
-	    $fetch_headers = explode(',', $fetch_headers_str);
+	    $fetch_headers = array_merge(['URL', 'HTTP Status Code'],
+		    explode(',', $fetch_headers_str));
+	    var_dump($fetch_headers);
 	    $this->info("Hello $name");
 	    $this->info("Headers ${fetch_headers_str}");
+	    $response = $this->doRequest('GET');
+	    $val = [];
+	    foreach ( $fetch_headers as $header_name ) {
+	    	if ( $response instanceof Response ) {
+			    $val[ $header_name ] = implode(', ', $response->getHeader( $header_name ));
+		    }
+	    }
+	    $val['URL'] = 'http://www.l2tp.org';
+	    $val['HTTP Status Code'] = $response->getStatusCode();
+	    var_dump($fetch_headers, $val);
+	    $this->table($fetch_headers,
+	    	$val);
 	    return true;
     }
 
-    private function doRequest() {
-	    $base_url = 'http://example.com';
+    private function doRequest($method) {
+	    $base_url = 'http://www.l2tp.org';
 	    $client = new Client( [
 		    'base_uri' => $base_url,
 	    ] );
 
 	    $path = '/index.html';
-	    $response = $client->request( 'GET', $path,
-		    [
-			    'allow_redirects' => true,
-		    ] );
-	    $response_body = (string) $response->getStatusCode();
-	    echo $response_body;
+	    try {
+		    $response = $client->request( $method, $path,
+			    [
+				    'allow_redirects' => false,
+			    ] );
+	    } catch (ClientException $e) {
+	    	$response = $e->getResponse();
+	    	// do nothing
+	    }
+	    return $response;
     }
 }
